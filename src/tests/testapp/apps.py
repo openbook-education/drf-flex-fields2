aa59cf0cc6065880b8c40783f3848be9cf9483ad
Dennis Schulmeister-Zimolong <dennis@patk.org>
2026-03-31 15:41:54 +0200

    src/tests/testapp/apps.py: Copilot stylistic warnings

--- src/tests/testapp/apps.py
+++ src/tests/testapp/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class TestappConfig(AppConfig):
-    """Mock Django application used for unit tests"""
-    name = 'tests.testapp'
+    """Mock Django application used for unit tests."""
+    name = "tests.testapp"
